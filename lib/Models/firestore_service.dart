f3aa99aa3c7021d1cc67eb1be8906c9421beaa24
SaadAkram200 <saadakram200@gmail.com>
2023-11-22 09:49:27 +0500

    working on profile screen

--- lib/Models/firestore_service.dart
+++ lib/Models/firestore_service.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:sebet/Models/user_model.dart';
+import 'package:sebet/models/user_model.dart';
 
 class FirestoreServices {
   final CollectionReference<Map<String, dynamic>> users =
@@ -14,12 +14,24 @@ class FirestoreServices {
     return doc.set(user.toMap());
   }
 
+  //for signup2 screen
     Future<void> updateUserData(String state, String zipcode, String city, String address) {
     var uid = FirebaseAuth.instance.currentUser!.uid;
     return users.doc(uid).update({
       'state': state,
       'zipcode': zipcode,
       'city': city,
+      'address': address,
+    });
+  }
+
+  // for edit profile
+    Future<void> editUserData(String firstname, String lastname, String phone, String address) {
+    var uid = FirebaseAuth.instance.currentUser!.uid;
+    return users.doc(uid).update({
+      'firstname': firstname,
+      'lastname': lastname,
+      'phone': phone,
       'address': address,
     });
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:sebet/Sebet/profile_page.dart';
-import 'package:sebet/Sebet/splash_screen.dart';
+import 'package:sebet/screens/profile_page.dart';
+import 'package:sebet/screens/splash_screen.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
